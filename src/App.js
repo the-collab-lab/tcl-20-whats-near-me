d74191b11b17b4b5351a2fd937a3d73f06edce69
Rachel Fischoff <rfischoff@gmail.com>
2021-01-06 19:31:47 -0500

    feat: centered menu & h1 for views

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Help from './pages/Help';
 import Map from './pages/Map';
 import List from './pages/List';
