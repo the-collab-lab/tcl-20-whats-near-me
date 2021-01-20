cf97d94961f7fa73fc8a9d7976fdd61f728ef8e6
Shayne Sebro <shaynesebro@gmail.com>
2021-01-19 19:05:24 -0500

    added close functionality for info windows

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default function InfoWindow({ text, $dimensionKey }) {
+  const handleClose = () => {
+    document.querySelector(`.info-window-${$dimensionKey}`).style.display =
+      'none';
+  };
+
   return (
     <div
       className={`info-window-${$dimensionKey}`}
@@ -12,7 +17,7 @@ export default function InfoWindow({ text, $dimensionKey }) {
         display: 'none',
       }}
     >
-      <button>x</button>
+      <button onClick={handleClose}>x</button>
     </div>
   );
 }
