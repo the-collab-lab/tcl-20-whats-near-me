649cf1df39f0fd37be6568a381bdb6fa0b8993a1
Caitlyn <cgreffly@gmail.com>
2021-01-23 10:42:30 -0800

    change table to basic table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,54 +1,30 @@
 import React from 'react';
-
-export default class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log({ props });
-    this.getHeader = this.getHeader.bind(this);
-    this.getRowsData = this.getRowsData.bind(this);
-    this.getKeys = this.getKeys.bind(this);
-  }
-
-  getKeys = function () {
-    return Object.keys(this.props.data[0]);
-  };
-
-  getHeader = function () {
-    var keys = this.getKeys();
-    return keys.map((key, index) => {
-      return <th key={key}>{key.toUpperCase()}</th>;
-    });
-  };
-
-  getRowsData = function () {
-    var items = this.props.data;
-    var keys = this.getKeys();
-    return items.map((row, index) => {
-      return (
-        <tr key={index}>
-          <RenderRow key={index} data={row} keys={keys} />
-        </tr>
-      );
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>hey</h1>
-        <table>
-          <thead>
-            <tr>{this.getHeader()}</tr>
-          </thead>
-          <tbody>{this.getRowsData()}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-const RenderRow = (props) => {
-  return props.keys.map((key, index) => {
-    return <td key={props.data[key]}>{props.data[key]}</td>;
-  });
+const Row = ({ location }) => {
+  return (
+    <tr>
+      <td>{location.title}</td>
+    </tr>
+  );
 };
+const Table = ({ data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Location</td>
+          <td>Description</td>
+          <td>Link</td>
+          <td>Image</td>
+        </tr>
+      </thead>
+      <tbody>
+        {data &&
+          data.map((location) => {
+            return <Row key={location.pageid} location={location} />;
+          })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
