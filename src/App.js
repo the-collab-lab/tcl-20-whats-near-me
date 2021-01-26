a0fd490b12607810c6965819479d1acef686a489
Caitlyn <cgreffly@gmail.com>
2021-01-25 17:52:01 -0800

    Merge branch 'main' into cg-dt-create-table, sort table by distance, use context in table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LocationsContextProvider from './context/LocationsContext';
 import Help from './pages/Help';
