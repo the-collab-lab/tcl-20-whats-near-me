dfdbc643d200d07b4f98dd7f15004e6537a5ac89
Rachel Fischoff <rfischoff@gmail.com>
2021-02-04 19:39:57 -0500

    working on search.js component filtered list

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Search.css';
+import { LocationsContext } from '../context/LocationsContext';
 
 export default function Search() {
+  const { locations } = useContext(LocationsContext);
+
+  const [filteredLocations, setFilteredLocations] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(null);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+
+    setFilteredLocations();
+
+    // useEffect(() => {
+    //   let listFilter = currentList.userList;
+    //   let filtered =
+    //     listFilter &&
+    //     listFilter.filter((item) => {
+    //       return item.itemName.toLowerCase().includes(filterValue.toLowerCase());
+    //     });
+    //   setFilteredList(filtered);
+    // }, [filterValue]);
+  };
+
+  const handleClear = () => {};
+
   return (
     <div className="searchBar">
-      <input type="text" placeholder="search" />
+      <input type="text" placeholder="search" onChange={handleSearch} />
     </div>
   );
 }
