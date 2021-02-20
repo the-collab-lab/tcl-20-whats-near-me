968979211f797cdb7c0be6687464bd4df3acbac3
Shayne Sebro <shaynesebro@gmail.com>
2021-02-20 15:02:43 -0500

    updating help page css

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Help.css';
 
 export default function Help() {
   return (
     <main>
       <h1>Help</h1>
+      <div className="about-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra
+        nec dolor sit amet pellentesque. Nam blandit lectus lacus, a malesuada
+        enim facilisis in. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Integer mattis nibh vel
+        congue pretium. Etiam sit amet lorem lacus. In sodales enim nunc, eget
+        sodales massa suscipit vel. Nunc sed enim lacus. Integer quis suscipit
+        nibh, ut pharetra nisl. Integer vel suscipit libero. Curabitur eget
+        scelerisque neque. Nulla facilisi. Fusce quis lectus justo. Maecenas non
+        iaculis lacus, ut sollicitudin orci.
+      </div>
+      <div className="explore-png"></div>
     </main>
   );
 }
